6a02dc709d74e46e250434788765fce29e2e9a70
ukshankar <ukshankar@gmail.com>
2017-03-19 15:44:08 -0400

    msg

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java -jar target/helloworld.jar
+web:    java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
---
+++ setenv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export M2_HOME=~/apache-maven-3.3.9
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home
+
+echo $JAVA_HOME
+echo $M2_HOME
+echo $M2
+echo $PATH
+
+mvn clean package -DskipTests=true
+
+git add .
+git commit -m "msg"
+git push heroku master
+
