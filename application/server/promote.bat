c2dc9ef596e25345a78cac1008d11465e145801f
vishalmysore <visrow@gmail.com>
2017-03-28 10:37:45 -0400

    msg

--- application/server/promote.bat
+++ application/server/promote.bat
@@ -2,7 +2,7 @@ cd ../client
 
 CALL ng build --env=prod
 
-cp -r dist/* ../service/src/main/webapp
+copy -r dist/* ../service/src/main/webapp
 
 cd ../service
 
@@ -10,7 +10,7 @@ CALL mvn -DENV=server clean install flyway:migrate
 
 cd ../server 
 
-cp ../service/target/HonuService.war .
+copy ../service/target/HonuService.war .
 CALL mvn clean package -DskipTests=true
 
 git add .
