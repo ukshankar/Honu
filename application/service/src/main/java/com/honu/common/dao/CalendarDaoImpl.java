112fe5b3f70b6256d2f0260c350b62cff0ef3f4f
vishalmysore <visrow@gmail.com>
2017-03-28 17:07:16 -0400

    refactored

--- application/service/src/main/java/com/honu/common/dao/CalendarDaoImpl.java
+++ application/service/src/main/java/com/honu/common/dao/CalendarDaoImpl.java
@@ -1,15 +1,9 @@
 package com.honu.common.dao;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
 
 import com.google.api.client.util.DateTime;
