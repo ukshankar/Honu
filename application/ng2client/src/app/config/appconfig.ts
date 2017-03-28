b70fe7f8702b150d31d649b065c9f2e49bcee58b
ukshankar <ukshankar@gmail.com>
2017-03-27 22:32:42 -0400

    update

---
+++ application/ng2client/src/app/config/appconfig.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AppConfig {
+
+  //serverHost='http://localhost:8888'
+  serverHost='{{env.service.host}}';
+
+  constructor() {}
+
+
+}
--- application/ng2client/src/app/config/index.ts
+++ application/ng2client/src/app/config/index.ts
@@ -1,12 +1,1 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class AppConfig {
-
-  //serverHost='http://localhost:8888'
-  serverHost=''
-
-  constructor() {}
-
-
-}
+export * from './appconfig';
