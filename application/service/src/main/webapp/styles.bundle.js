9832df8752d4b2ebf2cdb2105d4347a0aa5c520c
ukshankar <ukshankar@gmail.com>
2017-03-30 23:31:12 -0400

    fix

--- application/service/src/main/webapp/styles.bundle.js
+++ application/service/src/main/webapp/styles.bundle.js
@@ -65,7 +65,7 @@ exports = module.exports = __webpack_require__(14)();
 
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\n", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\n", ""]);
 
 // exports
 
