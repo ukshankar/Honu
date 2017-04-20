c9476a620ae9ecabd1b2b2f70c02c16713b70d2b
ukshankar <ukshankar@gmail.com>
2017-04-20 19:54:03 -0400

    test case added

--- application/client/e2e/app.e2e-spec.ts
+++ application/client/e2e/app.e2e-spec.ts
@@ -1,14 +1,35 @@
-import { ClientPage } from './app.po';
+import { HomePage } from './app.po';
 
-describe('client App', () => {
-  let page: ClientPage;
+describe('Honu App', () => {
+  let page: HomePage;
 
   beforeEach(() => {
-    page = new ClientPage();
+    page = new HomePage();
   });
 
-  it('should display message saying app works', () => {
+  it('Should have title Honu', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getHeaderTag().getTagName()).toBe('a');
   });
+
+  it('Should have 4 button', () => {
+    let promise= page.getButtons();
+    promise.then(function(buttons){
+      expect(buttons[0].getText()).toBe('Mock Interview');
+      expect(buttons[1].getText()).toBe('Resume & Cover Letter');
+      expect(buttons[2].getText()).toBe('Career Advice');
+      expect(buttons[3].getText()).toBe('Coach & Mentor');
+    });
+   
+  });
+
+  it('Go to Signin page', () => {
+    let promise= page.getButtons();
+    promise.then(function(buttons){
+      buttons[0].click();
+        expect(page.getCurrentUrl()).toEqual('http://localhost:49152/#/signon');
+    });
+   
+  });
+
 });
--- application/client/e2e/app.po.ts
+++ application/client/e2e/app.po.ts
@@ -1,11 +1,28 @@
 import { browser, element, by } from 'protractor';
 
-export class ClientPage {
+export class HomePage {
+  constructor(){
+      by.addLocator('formControlName', function(value, opt_parentElement, opt_rootSelector) {
+        var using = opt_parentElement || document;
+        return using.querySelectorAll('[formControlName="' + value +'"]');
+      });
+  }
   navigateTo() {
+
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getHeaderTag() {
+    return element(by.linkText('HONU'));
   }
+
+  getButtons() {
+    return element.all(by.tagName('button'));
+  }
+
+  getCurrentUrl(){
+    return browser.getCurrentUrl();
+  }
+
+
 }
